071dec022d3d34501805100a6b4239266da47855
sekkey777 <seki7united@gmail.com>
2022-12-31 00:12:55 +0900

    機能追加：ヘッダー
    ・設定ボタン（コメントアウト中）
    ・設定メニューの追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,19 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// function settingButton(el){
+//   console.log(el.style.display)
+//   if (el.style.display=='') {
+//     el.style.display='none';
+//   } else {
+//     el.style.display='';
+//   }
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const button = document.querySelector('.setting-button');
+//   const navigation = document.querySelector('.navigasion-box');
+//   navigation.style.display = 'none';
+//   button.addEventListener("click", () => { settingButton(navigation) })
+// })
